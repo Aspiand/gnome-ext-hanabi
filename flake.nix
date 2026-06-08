be03bd3da40370cc5a9f77942343ecf92d4e9499
Muhammad Aspian <aspiand@users.noreply.github.com>
2026-06-08 06:28:02 +0000

    refactor: inline package into flake.nix, remove nix/package.nix

--- flake.nix
+++ flake.nix
@@ -11,32 +11,91 @@
 
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
 
-      # Version from the source repo itself
       version =
         if self ? lastModifiedDate && self ? shortRev then
           "${builtins.substring 0 8 self.lastModifiedDate}-${self.shortRev}"
         else
           "unstable";
 
-      hanabiPackage = pkgs:
-        pkgs.callPackage ./nix/package.nix {
-          src = self.outPath;
-          inherit version;
+      extensionName = "hanabi";
+      extensionUuid = "hanabi-extension@jeffshee.github.io";
+
+      mkHanabi = pkgs: pkgs.stdenv.mkDerivation {
+        pname = "gnome-ext-hanabi";
+        inherit version;
+        src = self.outPath;
+
+        nativeBuildInputs = with pkgs; [
+          meson
+          ninja
+          glib
+          nodejs
+          wrapGAppsHook4
+          appstream-glib
+          gobject-introspection
+          shared-mime-info
+        ];
+
+        buildInputs = with pkgs; [
+          gst_all_1.gstreamer
+          gst_all_1.gst-plugins-base
+          gst_all_1.gst-plugins-good
+          gst_all_1.gst-plugins-bad
+          gst_all_1.gst-plugins-ugly
+          gst_all_1.gst-libav
+          gst_all_1.gst-vaapi
+          clapper
+          gjs
+          gtk4
+          glib
+          wayland
+          wayland-protocols
+        ];
+
+        dontWrapGApps = true;
+
+        postPatch = ''
+          patchShebangs build-aux/meson-postinstall.sh
+        '';
+
+        postFixup = ''
+          wrapGApp "$out/share/gnome-shell/extensions/${extensionUuid}/renderer/renderer.js"
+
+          ln -s "$out/share/gsettings-schemas/${pname}-${version}/glib-2.0/schemas" \
+            "$out/share/gnome-shell/extensions/${extensionUuid}/schemas"
+        '';
+
+        passthru = {
+          inherit extensionName extensionUuid;
         };
+
+        meta = with pkgs.lib; {
+          description = "GNOME Shell extension for animated video wallpapers";
+          longDescription = ''
+            Hanabi (花火, fireworks) is a GNOME Shell extension that lets you use any
+            video or GIF as your desktop wallpaper. It supports both X11 and Wayland,
+            and takes advantage of hardware-accelerated video playback via GStreamer
+            and Clapper.
+          '';
+          homepage = "https://github.com/jeffshee/gnome-ext-hanabi";
+          changelog = "https://github.com/jeffshee/gnome-ext-hanabi/releases";
+          license = licenses.gpl3Plus;
+          platforms = platforms.linux;
+          maintainers = with maintainers; [ ];
+        };
+      };
     in {
       packages = forAllSystems (system:
-        let pkgs = import nixpkgs { inherit system; };
+        let
+          hanabi = mkHanabi (import nixpkgs { inherit system; });
         in {
-          default = hanabiPackage pkgs;
-          hanabi = hanabiPackage pkgs;
+          inherit hanabi;
+          default = hanabi;
         }
       );
 
       overlays.default = final: _: {
-        hanabi = final.callPackage ./nix/package.nix {
-          src = self.outPath;
-          inherit version;
-        };
+        hanabi = mkHanabi final;
       };
     };
 }
